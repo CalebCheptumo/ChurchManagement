6412ad403669b0d7c62c24321fe9a3ae7db622d8
calebcheptumo <ckibusia@gmail.com>
2024-01-01 11:57:28 +0300

    Created pastor model

--- ChurchManagment/Pastor/models.py
+++ ChurchManagment/Pastor/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Pastor(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    contact_number = models.CharField(max_length=20)
+    pastoral_history = models.TextField()
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
