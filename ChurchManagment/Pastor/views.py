fc3a1b87d33a6d3bfedb12f057c87126c2225640
calebcheptumo <ckibusia@gmail.com>
2024-01-01 12:04:14 +0300

    Added Pastor views

--- ChurchManagment/Pastor/views.py
+++ ChurchManagment/Pastor/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.views import generic
+from .models import Pastor
 
-# Create your views here.
+class PastorListView(generic.ListView):
+    model = Pastor
+    template_name = 'pastor_list.html'
+
+class PastorDetailView(generic.DetailView):
+    model = Pastor
+    template_name = 'pastor_detail.html'
+
+class PastorCreateView(generic.CreateView):
+    model = Pastor
+    fields = ['first_name', 'last_name', 'contact_number', 'pastoral_history']
+    template_name = 'pastor_form.html'
+
+class PastorUpdateView(generic.UpdateView):
+    model = Pastor
+    fields = ['first_name', 'last_name', 'contact_number', 'pastoral_history']
+    template_name = 'pastor_form.html'
+
+class PastorDeleteView(generic.DeleteView):
+    model = Pastor
+    template_name = 'pastor_confirm_delete.html'
+    success_url = '/pastors/'
